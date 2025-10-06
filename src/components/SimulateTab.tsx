303129077a9f153200a8a6eb1eb0ffd174cf3045
Doniyor Alimov <alimov.doniyor28@gmail.com>
2025-10-06 20:11:09 +0500

    Setup Tanstack/React-Query

--- src/components/SimulateTab.tsx
+++ src/components/SimulateTab.tsx
@@ -1,36 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { CiPlay1 } from "react-icons/ci";
-import { CiPause1 } from "react-icons/ci";
+import { CiPause1, CiPlay1 } from "react-icons/ci";
 
 const SimulateTab = () => {
-  const [isRunning, setIsRunning] = useState(false);
+  const queryKey = "status";
+  const queryClient = useQueryClient();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/status")
-      .then((res) => res.data)
-      .then((status) => setIsRunning(status));
-  }, []);
+  const { data: isRunning } = useQuery({
+    queryKey: [queryKey],
+    queryFn: () =>
+      axios.get("http://localhost:8000/status").then((res) => res.data),
+  });
 
-  const handleStart = () => {
-    axios.post("http://localhost:8000/start").then(() => setIsRunning(true));
-  };
-
-  const handleStop = () => {
-    axios.post("http://localhost:8000/stop").then(() => setIsRunning(false));
-  };
+  const startSimulation = useMutation({
+    mutationFn: () => axios.post("http://localhost:8000/start"),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [queryKey] }),
+  });
+  const stopSimulation = useMutation({
+    mutationFn: () => axios.post("http://localhost:8000/stop"),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [queryKey] }),
+  });
 
   return (
     <div className=" text-3xl flex gap-2 items-center">
       {isRunning ? (
         <CiPause1
-          onClick={handleStop}
+          onClick={() => stopSimulation.mutate()}
           className="text-amber-500 cursor-pointer"
         />
       ) : (
         <CiPlay1
-          onClick={handleStart}
+          onClick={() => startSimulation.mutate()}
           className="text-green-500 cursor-pointer"
         />
       )}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@xyflow/react/dist/style.css";
 import "./index.css";
 import App from "./App";
 
+const client = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
