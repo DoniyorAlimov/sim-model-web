2cc10cde71cf1ecaa37b4cc09cadf0e1cdcc705e
Doniyor Alimov <alimov.doniyor28@gmail.com>
2025-10-06 16:32:13 +0500

    Connect simulation to the backend

--- src/components/SimulateTab.tsx
+++ src/components/SimulateTab.tsx
@@ -1,28 +1,40 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { CiPlay1 } from "react-icons/ci";
 import { CiPause1 } from "react-icons/ci";
 
 const SimulateTab = () => {
   const [isRunning, setIsRunning] = useState(false);
 
-  const handleClick = () => {
-    setIsRunning(!isRunning);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/status")
+      .then((res) => res.data)
+      .then((status) => setIsRunning(status));
+  }, []);
+
+  const handleStart = () => {
+    axios.post("http://localhost:8000/start").then(() => setIsRunning(true));
+  };
+
+  const handleStop = () => {
+    axios.post("http://localhost:8000/stop").then(() => setIsRunning(false));
   };
 
   return (
     <div className=" text-3xl flex gap-2 items-center">
       {isRunning ? (
-        <CiPlay1
-          onClick={handleClick}
-          className="text-green-500 cursor-pointer"
-        />
-      ) : (
         <CiPause1
-          onClick={handleClick}
+          onClick={handleStop}
           className="text-amber-500 cursor-pointer"
         />
+      ) : (
+        <CiPlay1
+          onClick={handleStart}
+          className="text-green-500 cursor-pointer"
+        />
       )}
-      {isRunning ? <div>Play</div> : <div>Pause</div>}
+      {isRunning ? <div>Pause</div> : <div>Play</div>}
     </div>
   );
 };
